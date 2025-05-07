42ccfb8f090ad434d7234821136e98eaa8872479
John Haynes <john.andrew.haynes@gmail.com>
2025-05-07 17:29:39 +0200

    db

--- Acid/Acid/Config/Db.cs
+++ Acid/Acid/Config/Db.cs
@@ -2,5 +2,5 @@ namespace Acid.Config;
 
 public class Db
 {
-    public const string Conn = "Data Source=tcp:127.0.0.1;User=sa;Password=aL4oTec3nu9nooW9";
+    public const string Conn = "Data Source=tcp:127.0.0.1;User=sa;Password=eimu9ieGYi8ahque";
 }
