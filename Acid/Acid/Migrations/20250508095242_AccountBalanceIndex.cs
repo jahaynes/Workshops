eabfc165a02112a0e71f9c19d5eae12fb64830b7
John Haynes <john.h@bokio.se>
2025-05-08 11:54:58 +0200

    foo

---
+++ Acid/Acid/Migrations/20250508095242_AccountBalanceIndex.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acid.Migrations
+{
+    public partial class AccountBalanceIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_Balance",
+                table: "MyAccounts",
+                column: "Balance");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Account_Balance",
+                table: "MyAccounts");
+        }
+    }
+}
