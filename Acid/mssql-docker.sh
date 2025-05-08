8f4c4fefb5199c37386dc18f3878246481a2d03d
John Haynes <john.h@bokio.se>
2025-05-08 10:26:27 +0200

    db & migration stuff

--- Acid/mssql-docker.sh
+++ Acid/mssql-docker.sh
@@ -1,1 +1,1 @@
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=eimu9ieGYi8ahque" -e "MSSQL_PID=Evaluation" -p 1433:1433  --name acid-experiment --hostname acid-experiment mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=eimu9ieGYi8ahque" -e "MSSQL_PID=Evaluation" -p 1433:1433  --name acid-experiment --hostname acid-experiment mcr.microsoft.com/mssql/server:2022-latest
